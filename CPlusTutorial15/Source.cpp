576a70c2828ef49861f2f14ba9c8747247556156
NRowley <nicholas.ryan.rowley@gmail.com>
2023-12-08 12:58:02 -0600

    casting, static cast, and dynamic cast

--- CPlusTutorial15/Source.cpp
+++ CPlusTutorial15/Source.cpp
@@ -7,12 +7,20 @@ public:
 	virtual void BeginPlay() {
 		cout << "Object BeginPlay() called. \n";
 	}
+
+	void ObjectFunction() {
+		cout << "ObjectFunction() called.\n";
+	}
 };
 
 class Actor : public Object {
 public:
 	virtual void BeginPlay() override {
 		cout << "Actor BeginPlay() called \n";
+	}
+
+	void ActorFunction() {
+		cout << "ActorFunction() called.\n";
 	}
 };
 
@@ -21,12 +29,22 @@ public:
 	virtual void BeginPlay() override {
 		cout << "Pawn BeginPlay() called \n";
 	}
+
+	void PawnFunction() {
+		cout << "PawnFunction() called.\n";
+	}
 };
+
+//CASTING
+
+
 
 
 
 int main()
 {
+	//POLYMORPHISM//////////////////////////
+
 	Object* ptr_to_object = new Object;
 	Actor* ptr_to_actor = new Actor;
 	Pawn* ptr_to_pawn = new Pawn;
@@ -34,8 +52,28 @@ int main()
 	Object* ObjectArray[] = { ptr_to_object, ptr_to_actor, ptr_to_pawn };
 
 	for (int i = 0; i < 3; i++) {
-		ObjectArray[i]->BeginPlay();
+		//ObjectArray[i]->BeginPlay();
+		
+		Object* obj = ObjectArray[i];
+
+		Actor* act = dynamic_cast<Actor*>(obj);
+		cout << i << endl;
+		if (act) {
+			act->ActorFunction();
+		}
+
+		Pawn* pwn = dynamic_cast<Pawn*>(obj);
+		cout << i << endl;
+		if (pwn) {
+			pwn->PawnFunction();
+		}
 	}
+
+
+	//CASTING///////////////////////////////////////////
+
+
+
 
 	delete ptr_to_actor;
 	delete ptr_to_pawn;
